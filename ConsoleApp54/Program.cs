df614d1d4caba5df49d3f0a06b6252ef59464bef
Denis Tokmenko <98480275+ATWA66@users.noreply.github.com>
2022-09-18 21:02:34 +0300

    Add files via upload

---
+++ ConsoleApp54/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp54
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Console.WriteLine(Book.Notes.AddNote("lll"));
+        }
+        class Book
+        {
+            public static class Notes
+            {
+                public static string notes { get; set; } = "notes:";
+                public static string AddNote(string note)
+                {
+                    notes += note;
+                    return notes;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp55/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp55
+{
+    static class Ext
+    {
+        static public int[] HSort(this int[] nums)
+        {
+            int temp;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+            return nums;
+        }
+    }
+    internal class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 5, 4, 7, 23, 9 };
+            a.HSort();
+            for(int i = 0; i< a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
